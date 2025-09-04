8bb281f88de6a6d9207a4146dd2ba84161ca8ade
saiganesh74 <ganeshrejeti1@gmail.com>
2025-09-04 13:36:06 +0530

    Combination of messages

---
+++ mixed.sh
@@ -0,0 +1,11 @@
+######################################################
+# This shell file is a combination to run the script #
+######################################################
+
+#!/bin/bash
+echo 'Going into NaviLoop folder'
+cd ~/NaviLoop/
+
+#Printing the contents in README file 
+echo 'Printing the contents in README file '
+cat README.md
